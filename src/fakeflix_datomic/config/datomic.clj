7609491268654bf3f961f2241785076fcfa73916
Pedro Teles <pedroateles@protonmail.com>
2022-08-28 17:38:42 -0300

    feat!: add datomic implementation Add functions to insert and find schemas

---
+++ src/fakeflix_datomic/config/datomic.clj
@@ -0,0 +1,39 @@
+(ns fakeflix-datomic.config.datomic
+  (:require [datomic.api :as d]
+            [fakeflix-datomic.config.schema :as schema]
+            [schema.core :as s])
+  (:import (datomic Connection)))
+
+(def connection (atom nil))
+
+(s/defn transact-schemas!
+  [connection :- Connection
+   entity-schemas :- [{s/Keyword s/Keyword}]]
+  (d/transact connection entity-schemas))
+
+(s/defn create-connection!
+  [uri :- s/Str]
+  (swap! connection (fn [_] (d/connect uri))))
+
+(s/defn create-schemas :- [{s/Keyword s/Keyword}]
+  [schemas :- [{s/Any s/Any}]]
+  (->> schemas
+       (map schema/entity-schema)
+       (reduce into)))
+
+(s/defn start-datomic
+  ([host :- s/Str
+    db-name :- s/Str
+    password :- s/Str
+    type :- s/Keyword
+    schemas :- [{s/Any s/Any}]]
+   (let [uri (str "datomic:" (name type)  "://" host "/" db-name "?password=" password)
+         entity-schemas (create-schemas schemas)]
+     (d/create-database uri)
+     (create-connection! uri)
+     (transact-schemas! @connection entity-schemas)))
+  ([db-name :- s/Str
+    password :- s/Str
+    type :- s/Keyword
+    schemas :- [{s/Any s/Any}]]
+   (start-datomic "localhost:4334" db-name password type schemas)))
--- src/fakeflix_datomic/core.clj
+++
@@ -1,1 +0,0 @@
-(ns fakeflix-datomic.core)

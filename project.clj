86b36085eb711fbae8ab7157baf2d395d9b46afc
Pedro Teles <pedroateles@protonmail.com>
2022-08-28 15:26:07 -0300

    feat!: initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.pedroso/fakeflix-datomic "1.0.0"
+  :description "Fakeflix Datomic library"
+  :url "https://pedroteles.dev"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns fakeflix-datomic.core})
